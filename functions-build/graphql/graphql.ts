ffac5e0dd4788f1bb01adfc2654ca6df2ddbfeeb
Cagatay Kaydir <cagataykaydir@gmail.com>
2021-06-22 01:04:03 +0300

    minor changes

--- functions-build/graphql/graphql.ts
+++ functions-build/graphql/graphql.ts
@@ -1,21 +1,21 @@
-const { ApolloServer, gql } = require('apollo-server-lambda')
+import { ApolloServer, gql } from 'apollo-server-lambda';
 
 const typeDefs = gql`
   type Query {
     hello: String
   }
-`
+`;
+
 const resolvers = {
   Query: {
     hello: () => 'Hello, lskafj!',
   },
-}
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-})
+});
 
-const handler = server.createHandler()
-
-module.exports = { handler }
+// eslint-disable-next-line import/prefer-default-export
+export const handler = server.createHandler();
