70098e3fc387f8dfa2690642b97a6c6d8ba97c8f
Cagatay Kaydir <cagataykaydir@gmail.com>
2021-06-21 17:01:30 +0300

    typescript

---
+++ functions-build/apollo-graphql/apollo-graphql.ts
@@ -0,0 +1,21 @@
+const { ApolloServer, gql } = require('apollo-server-lambda')
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, world!',
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const handler = server.createHandler()
+
+module.exports = { handler }
