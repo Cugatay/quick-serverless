71f998c43d4fdee24444cd4ac5167dc5f64f256c
Cagatay Kaydir <cagataykaydir@gmail.com>
2021-06-22 01:14:29 +0300

    minor change

--- functions-build/graphql.ts
+++ functions-build/graphql.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from 'apollo-server-lambda';
 
-import typeDefs from '../../typeDefs';
-import resolvers from '../../resolvers';
+import typeDefs from '../typeDefs';
+import resolvers from '../resolvers';
 
 const server = new ApolloServer({
   typeDefs,
